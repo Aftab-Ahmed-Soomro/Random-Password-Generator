50b5cc7208dae4cb00e5fb45f1dda9582fb7a054
Aftab-Ahmed-Soomro <aftab.ahmeddailyideas@gmail.com>
2024-05-22 01:14:58 -0700

    added files

---
+++ app.js
@@ -0,0 +1,26 @@
+let passwordBox = document.getElementById('password');
+let length = 12;
+
+let uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+let lowercase = 'abcdefghijklmnopqrstuvwxyz';
+let number = '0123456789';
+let symbol = '!@#$%^&*()_+-=[]{}|;:<>,./?';
+let allChars = uppercase + lowercase + number + symbol;
+
+function generatePassword() {
+    let password = '';
+    password += uppercase[Math.floor(Math.random() * uppercase.length)];
+    password += lowercase[Math.floor(Math.random() * lowercase.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+    password += symbol[Math.floor(Math.random() * symbol.length)];
+
+    while (length > password.length) {
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+    passwordBox.value = password;
+}
+
+function copyPassword() {
+    passwordBox.select();
+    document.execCommand('copy');
+}
